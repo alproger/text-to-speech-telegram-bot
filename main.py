a6d1af4faa17e57f48cb17d8732acdb7e72335cb
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-12-06 10:12:30 +0500

    main changed and fixed bugs

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ questions = {
                  'answer3'  : 'Какие механизмы существует в распознавание речи?',
                  'answer4'  : 'Что такое Sonix?'
 }
-
 def text_eq(text1, text2):
     result = difflib.SequenceMatcher(None,f'{text1}',f'{text2}').ratio()
     return result
 
-
-bot = telebot.TeleBot("2129516170:AAGAjo7Pui79YBCIc-SyWBbMEU14q35qWx4")
+bot = telebot.TeleBot("token here")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	answer = 'Hello I can convert text to speech in russian and english languages.\nSend me text'
@@ -28,7 +26,7 @@ def echo_all(message):
         keys = [k for k, v in questions.items() if v == message.text]
         key = keys[0]
         print(key)
-        audio = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/{key}.wav', 'rb')
+        audio = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/answers/{key}.wav', 'rb')
         bot.send_audio(message.chat.id, audio = audio)
     except:
         message_lang = lang_detecter(message.text[0:50])
@@ -37,35 +35,10 @@ def echo_all(message):
             bot.send_message(message.chat.id, text='Audio is saving...')
             text_to_speech = gTTS(text=message.text, lang=message_lang, slow=False)
             text_to_speech.save(f"{audio_name}.wav")
-            audio_file = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/{audio_name}.wav', 'rb')	
+            audio_file = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/audios/{audio_name}.wav', 'rb')	
             bot.send_audio(message.chat.id, audio = audio_file)
         else:
             bot.send_message(message.chat.id, 'Please send me message in english or russian languages')
-    
-
-
-
-
-
-    # for key in questions.keys():
-    #     if text_eq(questions[key], message.text) > 0.9 :
-    #         percent = text_eq(questions[key], message.text)
-    #         print(percent)
-    #         audio = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/{key}.wav', 'rb')
-    #         bot.send_audio(message.chat.id, audio = audio)
-
-        # else:
-        #     if message_lang == 'ru' or message_lang == 'en':
-        #         audio_name = uuid4()
-        #         bot.send_message(message.chat.id, text='Audio is saving...')
-        #         text_to_speech = gTTS(text=message.text, lang=message_lang, slow=False)
-        #         text_to_speech.save(f"{audio_name}.wav")
-        #         audio_file = open(f'/home/alproger/Documents/GitHub/text-to-speech-telegram-bot/{audio_name}.wav', 'rb')	
-        #         bot.send_audio(message.chat.id, audio = audio_file)
-        #     else:
-        #         bot.send_message(message.chat.id, 'Please send me message in english or russian languages')
-    
-
 
 print('bot running...')
 bot.infinity_polling()
